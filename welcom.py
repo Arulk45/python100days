df55a2771f86d86cb99c0a83e096044b9021076e
Arul <arulswift007@gmail.com>
2024-04-04 17:59:35 +0530

    1=first test pro

--- welcom.py
+++ welcom.py
@@ -1,1 +1,9 @@
-print("Hello Py")
+# 
+#1. Create a greeting for your program.
+
+
+# 1st pro 
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
