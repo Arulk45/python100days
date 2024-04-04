c6b5542e185a999670b90c8b810c9ffe9eced308
Arul <arulswift007@gmail.com>
2024-04-04 13:41:43 +0530

    added prints examples

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ print("Hello World!")
 print("A 'single quote' inside a double quote")
 print('A "double quote" inside a single quote')
 print("Alternatively you can just \"escape\" the quote")
+
+#examples
+
+#print("She said: "Hello" and then left.")  SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print('She said: "Hello" and then left.')
+print("She said: \"Hello\" and then left.")
+
+
+print('This is for py test "program" !!!!! :) ')
