6af5a221195b4344ee136dc426efa0bd2844415e
Arul <arulswift007@gmail.com>
2024-04-04 14:49:15 +0530

    updated prints eg

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("A 'single quote' inside a double quote")
 print('A "double quote" inside a single quote')
 print("Alternatively you can just \"escape\" the quote")
 
-#examples
+#examples single & Double quotes
 
 #print("She said: "Hello" and then left.")  SyntaxError: invalid syntax. Perhaps you forgot a comma?
 print('She said: "Hello" and then left.')
@@ -11,3 +11,12 @@ print("She said: \"Hello\" and then left.")
 
 
 print('This is for py test "program" !!!!! :) ')
+
+
+
+print("Apple\nball\ncat")
+#String concatenation
+print("Hello" + "Arulkumar")
+print("Hello " + "Arulkumar")
+print("Hello" + " Arulkumar")
+print("Hello" + "  " + "Arulkumar")
