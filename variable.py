fbe7073295ca8ce2e99da06c0df618f95f867581
Arul <arulswift007@gmail.com>
2024-04-04 17:27:13 +0530

    var

---
+++ variable.py
@@ -0,0 +1,24 @@
+# name = input()
+# print(name)
+
+#2 pro
+# name = "baymax"
+# print(name)
+
+
+# name = input("Enter your name : ")
+# length = len(name)
+# print(length)
+
+#3 pro
+
+a = input()
+b = input()
+
+c = a
+a = b
+b = c
+
+print("A value is = " + a)
+print("B value is = " + b)
+
