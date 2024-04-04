537c74650254b06e82dd7113d711334de4573e4a
Arul <arulswift007@gmail.com>
2024-04-04 16:33:42 +0530

    input fun

--- inputfunction.py
+++ inputfunction.py
@@ -1,6 +1,19 @@
 
-#input function a rpompt fro the user
+# input function a rpompt fro the user
 
-#eg input("What is your name ?")
+# eg input("What is your name ?")
 
-print("Hello " + input("What is your name ?"))
+# print("Hello " + input("What is your name ?"))
+
+# print(input())
+
+
+a = int(input())
+b = int(input())
+
+print( a* b)
+
+
+c = len("Benjaimn")
+
+print(c)
