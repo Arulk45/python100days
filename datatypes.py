13c7220ce31a0bd69df69b5f2b99efbc03a91d2f
Arul <arulswift007@gmail.com>
2024-04-13 20:17:04 +0530

    Data types

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+#Data Types 
+
+#string
+#"Hello"
+
+print ("Hello"[1])
+
+
+print ("heelo" + "world")
+
+
+#integer
+
+print("123" + "567")
+
+123_456_789
+
+
+
+#Float
+
+3.14156
+
+#Boolean
+
+True  
+False
